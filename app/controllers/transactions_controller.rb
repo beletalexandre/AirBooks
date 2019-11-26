9936bafd684308071b5f34ec262a8e61f0327ee4
alexandre belet <belet.alexandre@gmail.com>
2019-11-26 12:32:34 +0100

    Transaction Controller Initiated

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,40 @@
 class TransactionsController < ApplicationController
+  def dashboard
+
+  end
+
+  def index
+    @book = Book.find(params[:book_id])
+    @transactions = Transaction.find(params[:user_id])
+  end
+
+  def new
+    @book = Book.find(params[:book_id])
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.user = current_user
+    @transaction.book = Book.find(params[:id])
+    @transaction.save
+
+    redirect_to transactions_path
+  end
+
+  def update
+    @transaction.update(transaction_params)
+  end
+
+  def destroy
+    @transaction.destroy
+
+    redirect_to transactions_path
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:date_begin, :end_date)
+  end
 end
