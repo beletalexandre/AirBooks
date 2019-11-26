02c1876926f3a570b9975d9d59a603d47a2555c8
alexandre belet <belet.alexandre@gmail.com>
2019-11-26 11:44:51 +0100

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
+  resources :books do
+    resources :transactions
+  end
+  get 'books/dashboard', to: 'books#dashboard', as: :dashboard
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
