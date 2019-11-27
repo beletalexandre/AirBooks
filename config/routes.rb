aa4f67bcd8a1ac5c1ab1495e9c583b8f92cbacee
Marc BORGNA <marc.borgna@gmail.com>
2019-11-27 22:16:12 +0100

    add places gem and modify users table

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
 get 'pages/dashboard', to: 'pages#dashboard', as: :dashboard
 get 'pages/dashboard/update/:status/:id', to: 'pages#update', as: :update_dashboard
-
+resources :users, only: ['edit', 'update']
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
